bc04785a95a3f5a36506646f8583d56025158eb9
ZAM <z2634541542@gmail.com>
2023-10-09 00:53:52 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (quantity : i32) -> i32{
+    let price = 2;
+    let mut total = price * quantity;
+    if quantity > 40 {
+        total = quantity
+    }
+    total
+}
 
 // Don't modify this function!
 #[test]
